b5bd636dc09d274f3aec6bf5127c26c0c83c4711
Branden Palmer <brandenpalmer08@gmail.com>
2021-08-08 15:49:27 +0700

    refactor to add running score change final score msg and font size add round counter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // store values for possible game moves for computer to select
-const gameMoveSelection = ['Rock', 'Paper', 'Scissors'];
+const gameMoveSelection = ["Rock", "Paper", "Scissors"];
 
 // initialized each players scores as a global
 let computerScore = 0;
@@ -13,21 +13,19 @@ let roundCounter = 0;
 let gameNeedsFinalScore = true;
 
 // initialize an element to display the results at bottom of page
-const container = document.querySelector('#container');
-const roundResultsContainer = document.createElement('div');
-const roundResultsText = document.createElement('div');
-roundResultsContainer.classList.add('round-results');
-roundResultsText.classList.add('round-results-text');
+const container = document.querySelector("#container");
+const roundResultsContainer = document.createElement("div");
+const roundResultsText = document.createElement("div");
+roundResultsContainer.classList.add("round-results");
+roundResultsText.classList.add("round-results-text");
 roundResultsContainer.append(roundResultsText);
 
-
 // initialize paper rock scissor buttons
-const divs = container.querySelectorAll('div');
+const divs = container.querySelectorAll("div");
 // make paper rock scissors buttons call playMultipleRounds
 divs.forEach((div) => {
-    div.addEventListener('click', () => playMultipleRounds(div.id));
+    div.addEventListener("click", () => playMultipleRounds(div.id));
 });
-
 
 // get a random index for the computer to use for its move
 function getRandomIndex(indexSize) {
@@ -41,21 +39,25 @@ function computerPlay() {
 
 // display message if player wins in an appended div
 function userWins(userPlay, computerPlay) {
-    roundResultsText.textContent = `You win! ${userPlay} beats ${computerPlay}.`;
-    container.parentNode.append(roundResultsContainer);
     ++userScore;
+    roundResultsText.textContent = `You win! ${userPlay} beats ${computerPlay}. 
+    Score: ${userScore} win(s) ${computerScore} loss(es)`;
+    roundResultsText.textContent += ` Round ${computerScore + userScore}/5.`;
+    container.parentNode.append(roundResultsContainer);
 }
 
 // display message if player loses in an appended div
+// addition display running score and round number
 function userLoses(userPlay, computerPlay) {
-    roundResultsText.textContent = `You lose! ${computerPlay} beats ${userPlay}.`;
-    container.parentNode.append(roundResultsContainer);
     ++computerScore;
+    roundResultsText.textContent = `You lose! ${computerPlay} beats ${userPlay}. 
+    Score: ${userScore} win(s) ${computerScore} loss(es)`;
+    roundResultsText.textContent += ` Round ${computerScore + userScore}/5.`;
+    container.parentNode.append(roundResultsContainer);
 }
 
 // take user input and computer input and compare them declare round winner
 function playRound(userSelection, computerSelection) {
-
     // display msg if there is a tie and get input again
     while (userSelection == computerSelection) {
         computerSelection = computerPlay();
@@ -65,15 +67,20 @@ function playRound(userSelection, computerSelection) {
 
     // decide who wins and call appropriate func
     switch (userSelection) {
-        case 'Paper':
-            (computerSelection == "Rock") ?
-            userWins(userSelection, computerSelection): userLoses(userSelection, computerSelection);
+        case "Paper":
+            computerSelection == "Rock" ?
+                userWins(userSelection, computerSelection) :
+                userLoses(userSelection, computerSelection);
             break;
-        case 'Rock':
-            (computerSelection == "Scissors") ? userWins(userSelection, computerSelection): userLoses(userSelection, computerSelection);
+        case "Rock":
+            computerSelection == "Scissors" ?
+                userWins(userSelection, computerSelection) :
+                userLoses(userSelection, computerSelection);
             break;
-        case 'Scissors':
-            (computerSelection == "Paper") ? userWins(userSelection, computerSelection): userLoses(userSelection, computerSelection);
+        case "Scissors":
+            computerSelection == "Paper" ?
+                userWins(userSelection, computerSelection) :
+                userLoses(userSelection, computerSelection);
             break;
     }
 }
@@ -82,15 +89,17 @@ function playRound(userSelection, computerSelection) {
 function declareFinalWinner() {
     if (gameNeedsFinalScore) {
         // make reset button
-        const resetButton = document.createElement('button');
-        resetButton.id = 'reset-button';
-        resetButton.textContent = 'Play Again?';
-        resetButton.addEventListener('click', () => resetGame());
+        const resetButton = document.createElement("button");
+        resetButton.id = "reset-button";
+        resetButton.textContent = "Play Again?";
+        resetButton.addEventListener("click", () => resetGame());
 
         // make final score message
-        const finalWinner = document.createElement('div');
-        finalWinner.id = 'final-winner';
-        finalWinner.textContent = `Final Score: Computer-${computerScore} You-${userScore}`;
+        const finalWinner = document.createElement("div");
+        finalWinner.id = "final-winner";
+        userScore > computerScore ?
+            (finalWinner.textContent = `You are the Champion!`) :
+            (finalWinner.textContent = `The Computer is the Champion!`);
 
         // add both elements
         roundResultsContainer.append(finalWinner);
@@ -99,7 +108,6 @@ function declareFinalWinner() {
     gameNeedsFinalScore = false;
 }
 
-
 // remove final results at bottom then reset and start new game
 function resetGame() {
     // reset all counters
@@ -107,11 +115,11 @@ function resetGame() {
     userScore = 0;
     roundCounter = 0;
     gameNeedsFinalScore = true;
-    
+
     // remove round results and final score
-    const container = document.querySelector('#container');
-    const resetButton = document.getElementById('reset-button');
-    const finalWinner =  document.getElementById('final-winner');
+    const container = document.querySelector("#container");
+    const resetButton = document.getElementById("reset-button");
+    const finalWinner = document.getElementById("final-winner");
     container.parentNode.removeChild(roundResultsContainer);
     resetButton.remove();
     finalWinner.remove();
@@ -119,7 +127,7 @@ function resetGame() {
 
 // play number of rounds defined in MAX_ROUNDS then display final score on next call
 function playMultipleRounds(paperRockScissorsButtons) {
-    (roundCounter < MAX_ROUNDS) ? playRound(paperRockScissorsButtons, computerPlay()): declareFinalWinner();
+    roundCounter < MAX_ROUNDS ?
+        playRound(paperRockScissorsButtons, computerPlay()) :
+        declareFinalWinner();
 }
-
-
